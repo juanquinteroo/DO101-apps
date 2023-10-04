3313e1ec0ad2c034db8023462d0dc8cbab7792fc
juanquinteroo <juan.quinteroo@techrea.com>
2023-10-04 15:57:38 -0500

    update app v3

--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,2 +1,20 @@
 console.log("Hello World!\n");
 
+var response;
+
+app.get('/', function (req, res) {
+    response = 'This is version 2 of the app.' + '\n';
+
+    //send the response to the client
+    res.send(response);
+
+});
+
+var response;
+
+app.get('/', function (req, res) {
+    response = 'This is version 3 of the app.' + '\n';
+
+    //send the response to the client
+    res.send(response);
+  });
